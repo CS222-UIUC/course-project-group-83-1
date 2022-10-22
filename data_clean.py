c2bcb3564b986ae3d14771c658a52fc0461a3647
Claire <89149218+czchou2@users.noreply.github.com>
2022-10-21 19:48:07 -0500

    fix linting issues, combine data cleaning methods

--- data_clean.py
+++ data_clean.py
@@ -2,13 +2,14 @@ import re
 from nltk.stem.snowball import SnowballStemmer
 import spacy
 
+
 def cleanTweets(text):
     nlp = spacy.load("en_core_web_sm")
 
     text = regex(text).lower().split()
 
     # stem words to roots
-    stemmer = SnowballStemmer(language='english')  
+    stemmer = SnowballStemmer(language="english")
     stems = []
     for word in text:
         stems.append(stemmer.stem(word))
@@ -21,17 +22,18 @@ def cleanTweets(text):
 
     return ret
 
+
 def regex(text):
-    text = re.sub("'", "", text)                # keep contractions
+    text = re.sub("'", "", text)  # keep contractions
 
-    text = re.sub('@[A-Za-z0-9_]+', '', text)   # remove @mentions
-    text = re.sub('#[A-Za-z0-9_]+', '', text)   # remove #hashtags
+    text = re.sub("@[A-Za-z0-9_]+", "", text)  # remove @mentions
+    text = re.sub("#", "", text)  # remove #hashtags
 
-    text = re.sub(r'https\S+', '', text)        # remove urls
-    text = re.sub(r'www.\S+', '', text)
+    text = re.sub(r"https\S+", "", text)  # remove urls
+    text = re.sub(r"www.\S+", "", text)
 
-    text = re.sub('[^A-Za-z0-9]', ' ', text)    # remove non-alphanumeric chars
+    text = re.sub("[^A-Za-z0-9]", " ", text)  # remove non-alphanumeric chars
 
-    text = re.sub(r'\s+', ' ', text)            # remove extra whitespace
+    text = re.sub(r"\s+", " ", text)  # remove extra whitespace
 
     return text
--- test_clean.py
+++ test_clean.py
@@ -1,21 +1,24 @@
 import data_clean
 
+
 def test_regex():
-    assert data_clean.regex('i\'m don\'t can\'t') == 'im dont cant'
-    assert data_clean.regex('@abc #123') == ' '
-    assert data_clean.regex('https://regexr.com') == ''
-    assert data_clean.regex('www.google.com') == ''
-    assert data_clean.regex('`~!@#$%^&*()-=_+[]\\;\',./|:"<>?') == ' '
+    assert data_clean.regex("i'm don't can't") == "im dont cant"
+    assert data_clean.regex("@abc #123") == " 123"
+    assert data_clean.regex("https://regexr.com") == ""
+    assert data_clean.regex("www.google.com") == ""
+    assert data_clean.regex("`~!@#$%^&*()-=_+[]\\;',./|:\"<>?") == " "
+
 
 def test_cleanTweets():
     # test stem
-    text = 'he likes playing baseball and basketball but is not fond of running or tennis'
-    assert 'running' not in data_clean.cleanTweets(text)
-    assert 'play' in data_clean.cleanTweets(text)
+    text = """he likes playing baseball and basketball but is not fond of
+            running or tennis"""
+    assert "running" not in data_clean.cleanTweets(text)
+    assert "play" in data_clean.cleanTweets(text)
     # test stop
-    assert 'he' not in data_clean.cleanTweets(text)
-    assert 'and' not in data_clean.cleanTweets(text)
-    assert 'is' not in data_clean.cleanTweets(text)
-    assert 'of' not in data_clean.cleanTweets(text)
-    assert 'or' not in data_clean.cleanTweets(text)
-    assert 'run' in data_clean.cleanTweets(text)
+    assert "he" not in data_clean.cleanTweets(text)
+    assert "and" not in data_clean.cleanTweets(text)
+    assert "is" not in data_clean.cleanTweets(text)
+    assert "of" not in data_clean.cleanTweets(text)
+    assert "or" not in data_clean.cleanTweets(text)
+    assert "run" in data_clean.cleanTweets(text)
