157d6026bca38e1a354d1a1d5a969a937d974217
Jane Liu <janeliu2@illinois.edu>
2022-12-02 14:40:20 -0600

    formatting website, setting up pipeline

--- website_app/app.py
+++ website_app/app.py
@@ -1,21 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
 import scrape
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=('GET', 'POST'))
 def index():
-    """ alter this code to get the keyword submission
+    # alter this code to get the keyword submission
+    # include a way to access previous keyword searches
+    print("hi")
     if request.method == 'POST':
+        
         keyword = request.form['keyword']
         start_date = request.form['start_date']
-        
-        # (keyword, start_date, end_date, output_file)
-        if not title or (len(keyword) < 3):
+        end_date = request.form['end_date']
+        output_tag = "dummy_name.json" # try to make this have a unique id fo every process
+        # scrape_twitter(keyword, start_date, end_date, output_file)
+        if not keyword or (len(keyword) < 3):
             flash('valid keyword is required!')
         else:
-            # use scrape here
-    """
+            # 
+            # ----- check if this keyword has been used before
+                # ask whether the person would like to just load the results
+                # from the previous search
+            
+            #------ other wise conduct an entirely new scrape of twitter
+            
+            pass
+            #scrape_twitter(keyword, start_date, end_date, output_file)
+    
     return render_template('index.html')
 
 if __name__ == "__main__":
