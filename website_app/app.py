2cd489a469aa49ab8a6ef8f81a278f0816ba0ce5
Jane Liu <janeliu2@illinois.edu>
2022-12-02 17:03:30 -0600

    made the backend/ data pipeline

--- website_app/app.py
+++ website_app/app.py
@@ -1,8 +1,36 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
-import scrape
+import scrape, graph
+import sentiment_analysis as sen
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'admin123_foobar'
+
+def main_process(submit_data):
+    keyword, start_date, end_date, output_tag = submit_data
+    polarity_tags = ["Tweet", "Polarity", "Subjectivity"]
+    
+    scraped_data_folder = "scraped_data/"
+    full_polarity_folder = "processed_data/"
+    average_folder = "averaged_data/"
+    images_folder = "data_images/"
+
+    
+    scraped_output_file = scraped_data_folder + output_tag + "_scraped.json"
+    full_polarity_file = full_polarity_folder + output_tag + "_pol.json"
+    avg_polarity_file = average_folder + output_tag + "_avg.json"
+
+    scrape.scrape_twitter(keyword, start_date, end_date, scraped_output_file)
+    
+    sen.main_process_polarity(scraped_output_file, 
+                        polarity_tags, full_polarity_file, avg_polarity_file)
+    
+    # currently using the full_polarity file
+    graph.visualize_data(full_polarity_file, images_folder + output_tag)
+    
+
+
+
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
@@ -15,9 +43,14 @@ def index():
         start_date = request.form['start_date']
         end_date = request.form['end_date']
         output_tag = "dummy_name.json" # try to make this have a unique id fo every process
+
+
+        
         # scrape_twitter(keyword, start_date, end_date, output_file)
         if not keyword or (len(keyword) < 3):
-            flash('valid keyword is required!')
+            # deal with the invalid keyword here
+            # probably should deal with invalid dates too...?
+            pass
         else:
             # 
             # ----- check if this keyword has been used before
@@ -25,9 +58,9 @@ def index():
                 # from the previous search
             
             #------ other wise conduct an entirely new scrape of twitter
+            submit_data = keyword, start_date, end_date, output_tag
+            main_process(submit_data)
             
-            pass
-            #scrape_twitter(keyword, start_date, end_date, output_file)
     
     return render_template('index.html')
 
