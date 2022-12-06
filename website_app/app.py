986f002549eeb66ab344afd449cc6c65b9491f34
Jane Liu <janeliu2@illinois.edu>
2022-12-05 19:30:12 -0600

    errosr

--- website_app/app.py
+++ website_app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 import scrape, graph
 import sentiment_analysis as sen
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'admin123_foobar'
@@ -13,7 +14,7 @@ def main_process(submit_data):
     scraped_data_folder = "scraped_data/"
     full_polarity_folder = "processed_data/"
     average_folder = "averaged_data/"
-    images_folder = "data_images/"
+    images_folder = "static/images/"
 
     # change this part, put the "scraped" etc, tags at the beginning
     scraped_output_file = scraped_data_folder + "scraped_" + output_tag + ".json"
@@ -28,6 +29,9 @@ def main_process(submit_data):
     # currently using the full_polarity file
     graph.visualize_data(full_polarity_file, images_folder + output_tag)
     
+@app.route("/upload_image")
+def upload_image(filename):
+    return render_template("index.html", uploaded_image=filename)
 
 
 
@@ -36,13 +40,13 @@ def main_process(submit_data):
 def index():
     # alter this code to get the keyword submission
     # include a way to access previous keyword searches
-    print("hi")
+    
     if request.method == 'POST':
         
         keyword = request.form['keyword']
         start_date = request.form['start_date']
         end_date = request.form['end_date']
-        output_tag = "dummy_name.json" # try to make this have a unique id fo every process
+        output_tag = request.form['output_tag'] # try to make this have a unique id fo every process
 
 
         
@@ -50,19 +54,38 @@ def index():
         if not keyword or (len(keyword) < 3):
             # deal with the invalid keyword here
             # probably should deal with invalid dates too...?
-            pass
+            return render_template('index.html', error="Missing keyword.")
         elif not start_date or not end_date:
-
-            pass
-        else:
-            # 
-            # ----- check if this keyword has been used before
-                # ask whether the person would like to just load the results
-                # from the previous search
             
-            #------ other wise conduct an entirely new scrape of twitter
-            submit_data = keyword, start_date, end_date, output_tag
-            #main_process(submit_data)
+            return render_template('index.html', error="Missing valid dates.")
+        correctDate = None
+        try:
+            start = [ int(i) for i in start_date.split("-") ]
+            end = [ int(i) for i in end_date.split("-")]
+            startDate = datetime.datetime(start[0],start[1],start[2])
+            endDate = datetime.datetime(end[0],end[1],end[2])
+            if (endDate > startDate and len(start) < 4 and len(end) < 4):
+                correctDate = True
+            else:
+                correctDate = False
+        except Exception as e:
+            print(e)
+            correctDate = False
+             
+        if (not correctDate):
+            return render_template('index.html', error="Missing valid dates.")
+        # 
+        # ----- check if this keyword has been used before
+            # ask whether the person would like to just load the results
+            # from the previous search
+        
+        #------ other wise conduct an entirely new scrape of twitter
+        submit_data = keyword, start_date, end_date, output_tag
+        main_process(submit_data)
+        filename = output_tag + "_scatter_plot.png"
+        #filename = "images/scatter_plot.png"
+        
+        return render_template('index.html', uploaded_image = filename)
             
     
     return render_template('index.html')
